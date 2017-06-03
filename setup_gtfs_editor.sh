d0ff146dfcbde9ec7b750502f1a5bf542ed14e33
Tony Laidig <laidig@gmail.com>
2017-06-03 13:18:17 -0400

    update to point to repo with maven conveyal fix

--- setup_gtfs_editor.sh
+++ setup_gtfs_editor.sh
@@ -1,13 +1,13 @@
 wget -nc --quiet --progress=bar:force https://downloads.typesafe.com/play/1.2.6.2/play-1.2.6.2.zip
-unzip -u play-1.2.6.2.zip
+unzip -uo play-1.2.6.2.zip
 
-wget -O master.zip --quiet --progress=bar:force https://github.com/conveyal/gtfs-editor/archive/master.zip
-unzip -u master.zip
+wget -O master.zip --quiet --progress=bar:force https://github.com/laidig/gtfs-editor/archive/master.zip
+unzip -uo master.zip
 
 ls -hal
 
 cd gtfs-editor-master
-mkdir public/data/
+mkdir -p public/data/
 cp conf/application.conf.template conf/application.conf
 
 ../play-1.2.6.2/play dependencies
